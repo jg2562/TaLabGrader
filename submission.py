19ba83354bbf938ffb94080e348052474a81bf92
Jack Garrard <jg2562@nau.edu>
2017-08-18 02:15:32 -0700

    Added username to submission and fixed a few small errors

--- submission.py
+++ submission.py
@@ -3,7 +3,7 @@ import filecmp
 
 class Submission():
     def __init__(self, submission_dict):
-        self.username = ""
+        self.username = submission_dict["username"]
         self.student_name = submission_dict["name"]
         self.attachments = submission_dict["attachments"]
         self.comment = submission_dict["comment"]
@@ -16,15 +16,15 @@ class Submission():
             elif extension == "py":
                 self.code = attachment
 
-        self.partners = self._generate_partners()
+        self.partners = self._generate_partner_usernames()
 
     username_regex = re.compile("[a-zA-Z]{2,3}[0-9]{2,4}")
     def _generate_partner_usernames(self):
         code_fh = open(self.code, "r")
-        code = code_fh.readlines().join("\n")
-        matches = re.findall(username_regex, code)
-
-        return [match.group(0) for match in matches]
+        code = "\n".join(code_fh.readlines())
+        matches = re.findall(self.username_regex, code)
+        matches.remove(self.get_username())
+        return matches
 
     def get_name(self):
         return self.name
@@ -45,19 +45,19 @@ class Submission():
         return self.partners
 
     def is_partner(self, partner):
-        return is_bidirectional_partnership(student, partner) and has_same_attachments(partner)
+        return self.is_bidirectional_partnership(partner) and self.has_same_attachments(partner)
 
     def is_bidirectional_partnership(self, partner):
         return self.get_username() in partner.get_partners()
 
     def has_same_attachments(self, partner):
-        return has_same_code() and has_same_report()
+        return self.has_same_code(partner) and self.has_same_report(partner)
 
     def has_same_code(self, partner):
-        return is_same_file(self.get_code(), partner.get_code())
+        return self.is_same_file(self.get_code(), partner.get_code())
 
     def has_same_report(self, partner):
-        return is_same_file(self.get_report(), partner.get_report())
+        return self.is_same_file(self.get_report(), partner.get_report())
 
-    def is_same_file(file1, file2):
+    def is_same_file(self, file1, file2):
         return filecmp.cmp(file1, file2)
