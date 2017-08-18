aa688f763222085a05face6dffc9034382078889
Jack Garrard <jg2562@nau.edu>
2017-08-18 02:15:54 -0700

    Fixed several small errors in grouper and gave it runnable code

--- partnerGrouper.py
+++ partnerGrouper.py
@@ -1,17 +1,20 @@
+import json
+import filecmp
 from submission import Submission
+
 
 class PartnerGrouper():
     def __init__(self, submissions):
         self.submissions = {}
         for student in submissions:
-            self.submissions = Submission(submissions[student])
+            self.submissions[student] = Submission(submissions[student])
 
     def generate_groups(self):
         groups = []
         ungrouped_students = {student for student in submissions}
         while ungrouped_students:
             student = ungrouped_students.pop()
-            group = generate_group_from_student(student)
+            group = self.generate_group_from_student(student)
             groups.append(list(group))
             ungrouped_students -= group
         return groups
@@ -21,7 +24,14 @@ class PartnerGrouper():
         group.add(student)
         student_submission = self.submissions[student]
         for partner in student_submission.get_partners():
-            partner_sub = submissions[partner]
+            partner_sub = self.submissions[partner]
             if student_submission.is_partner(partner_sub):
                 group.add(partner)
         return group
+
+if __name__ == "__main__":
+    fh = open("students.json", "r")
+    submissions = json.load(fh)
+    grouper = PartnerGrouper(submissions)
+    groups = grouper.generate_groups()
+    print(json.dumps(groups))
