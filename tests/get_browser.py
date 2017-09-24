d8ffcfdb3bd87fe580a5ca21b9d3ccc0f3a8afa2
Jack Garrard <jg2562@nau.edu>
2017-09-24 00:10:53 -0700

    Add a test script to allow browser interaction

--- tests/get_browser.py
+++ tests/get_browser.py
@@ -1,1 +1,8 @@
+import code
+import grader.utils as utils
 from grader.browser.gradeBrowser import GradeBrowser
+
+if __name__ == "__main__":
+    config = utils.load_json("config/general.json")
+    browser = GradeBrowser(config)
+    code.interact(local=locals())
