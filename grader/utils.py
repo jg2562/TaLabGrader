32391b4354cc7e90a32da2eec30667732f31521f
Jack Garrard <jg2562@nau.edu>
2017-09-27 09:29:11 -0700

    Added ability to generate the groups and submissions from json or zip

--- grader/utils.py
+++ grader/utils.py
@@ -1,6 +1,8 @@
 import json
 from grader.submission.submission import Submission
+from grader.submission.submissionGenerator import SubmissionGenerator
 from grader.group.group import GroupSubmission
+from grader.group.partnerGrouper import PartnerGrouper
 
 def load_json(filename):
     fh = open(filename, "r")
@@ -17,9 +19,7 @@ def save_json(filename, data):
         fh.close()
 
 def convert_submission_dict_to_classes(submissions_dict):
-    class_submissions = {}
-    for student in submissions_dict:
-        class_submissions[student] = Submission(submissions_dict[student])
+    class_submissions = {k: Submission(submissions_dict[k]) for k in submissions_dict}
     return class_submissions
 
 def generate_group_submissions(submissions, groups):
@@ -28,3 +28,13 @@ def generate_group_submissions(submissions, groups):
         group = GroupSubmission(group_number, groups, submissions)
         new_groups[group_number] = group
     return new_groups
+
+def generate_group_and_submissions_from_json(config):
+    submissions = convert_submission_dict_to_classes(load_json(config['submissions json']))
+    groups = generate_group_submissions(submissions, load_json(config['groups json']))
+    return (submissions, groups)
+
+def generate_group_and_submissions_from_zip(config):
+    submissions = convert_submission_dict_to_classes(SubmissionGenerator(config).generate_submissions())
+    groups = generate_group_submissions(submissions, PartnerGrouper(submissions).generate_groups())
+    return (submissions, groups)
