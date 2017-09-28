206e00c823f21fc2b1b47bba9907cb5613108bff
Jack Garrard <jg2562@nau.edu>
2017-09-28 12:08:54 -0700

    Change initialize script to use grade browser to get user data

--- scripts/initialize.py
+++ scripts/initialize.py
@@ -1,37 +1,29 @@
 import pip
 import sys
 import csv
-import utils
+import grader.utils as utils
+import openpyxl
+from grader.browser.gradeBrowser import GradeBrowser
 
-
-username_file = 'usernames.json'
-workbook_name = 'grade-sheet.xlsx'
+config = utils.load_json("./config/general.json")
+workbook_name = config['spreadsheet']
 try:
     assert sys.version_info >= (3,0)
 except AssertionError:
     raise AssertionError("Must use python 3.0 or greater")
 
-# set up usernames json file
-usernames = {}
-comments_data = []
-with open('gradebook.csv') as csvfile:
-    csvreader = csv.reader(csvfile)
-    csvreader.__next__()
-    for row in csvreader:
-        usernames[row[1].strip() + " " + row[0].strip()] =  row[2]
-        comments_data.append((row[1].strip(), row[0].strip(), row[2].strip(), row[3].strip()))
-utils.save_json(username_file, usernames)
-
+# Get user data for comments sheet
+browser = GradeBrowser(config)
+comments_data = browser.get_user_data()
 
 # open and create grading sheet, check if rubric sheet exists
-import openpyxl
 workbook = openpyxl.load_workbook(workbook_name)
 workbook.active.title = "Rubric"
 comment_sheet = workbook.create_sheet("Comments", 0)
 comment_sheet.title = "Comments"
 for row, name in enumerate(sorted(comments_data, key=lambda x: x[1])):
-    comment_sheet.cell(row=row + 2, column=1).value = name[1]
-    comment_sheet.cell(row=row + 2, column=2).value = name[0]
+    comment_sheet.cell(row=row + 2, column=1).value = name[0]
+    comment_sheet.cell(row=row + 2, column=2).value = name[1]
     comment_sheet.cell(row=row + 2, column=3).value = name[2]
     comment_sheet.cell(row=row + 2, column=4).value = name[3]
 
@@ -45,3 +37,4 @@ for lab_num in range(12):
 
 workbook.save(workbook_name)
 workbook.close()
+browser.close()
