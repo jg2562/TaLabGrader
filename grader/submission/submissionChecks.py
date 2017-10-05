a60bc6f58d4d59eaa3dc05788677ad62a6274830
Jack Garrard <jg2562@nau.edu>
2017-10-05 10:11:17 -0700

    Changed checker for better modularity

--- grader/submission/submissionChecks.py
+++ grader/submission/submissionChecks.py
@@ -11,30 +11,36 @@ class GroupSubmissionGrader():
         groups_errors = {}
         for group_number in group_submissions:
             group = group_submissions[group_number]
-            groups_errors[group_number] = check_group(group)
+            groups_errors[group_number] = self.check_group(group)
         return groups_errors
 
     def check_group(self, group):
         code = group.get_code()
         if not code:
-            pep8 = (0, [])
+            pep8 = (0, "")
             syntax = False
         else:
             pep8 = self.check_pep8(code)
             syntax = self.check_syntax(code)
-        group_errors = {"Pep8": pep8, "Syntax": syntax}
+        group_errors = {"pep8": pep8, "syntax": syntax}
         return group_errors
 
     def check_pep8(self, code_file):
-        pep8_report = self.style_guide.check_files([code_file])
-        return (pep8_report.get_file_results(), pep8_report.get_statistics())
+        result = (0, [])
+        if code_file:
+            pep8_report = self.style_guide.check_files([code_file])
+            result = (int(pep8_report.get_file_results()), pep8_report.get_statistics())
+        return (result[0], "\n".join(result[1]))
 
     def check_syntax(self, code_file):
-        try:
-            py_compile.compile(code_file)
-            return True
-        except SyntaxError as e:
-            return False
+        result = False
+        if code_file:
+            try:
+                py_compile.compile(code_file)
+                result = True
+            except SyntaxError as e:
+                result = False
+        return (int(result), "Compilation " + ["Failed", "Succeeded"][result])
 
 if __name__ == "__main__":
     groups = utils.load_json("groups.json")
