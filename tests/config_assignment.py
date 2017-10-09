fe03a4534caeb8f7ef4d2bcb36e724989ab320fd
Jack Garrard <jg2562@nau.edu>
2017-10-09 15:16:51 -0700

    Changed test to use woth and possible instead of points

--- tests/config_assignment.py
+++ tests/config_assignment.py
@@ -2,12 +2,12 @@ import openpyxl
 import json
 
 if __name__ == "__main__":
-    row = 6
+    row = 8
     wb = openpyxl.load_workbook("./data/rubric.xlsx")
     sheet = wb.active
     cats = []
     for col in sheet[row][1:]:
         if col.value:
-            cat = {"name" : "", "description": col.comment.text, "points": int(col.value), "test": ""}
+            cat = {"name" : "", "description": col.comment.text, "worth": int(col.value), "possible": int(col.value), "test": ""}
             cats.append(cat)
     print(json.dumps(cats, indent=4, separators=(',', ': ')))
