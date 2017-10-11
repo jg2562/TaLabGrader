bdfe203eb34888b4f6c488aa1d5d96420978e70c
Jack Garrard <jg2562@nau.edu>
2017-10-10 23:20:03 -0700

    Switched when labgrader was called

--- scripts/setup_grades.py
+++ scripts/setup_grades.py
@@ -27,9 +27,9 @@ def students_to_sheet(config, submissions, lab_number):
     shutil.rmtree(groups_dir, ignore_errors=True)
     SubmissionSorter(submissions, groups).create_group_submissions(groups_dir)
     groups = utils.generate_group_submissions(submissions, groups)
-    group_grades = LabGrader(lab_config).get_groups_grades(groups)
-
     CommentGenerator().add_comments_to_sheet(submissions, lab_number, lab_sheet_name)
+
+    group_grades = LabGrader(lab_config).get_groups_grades(groups)
     GradeSheet(config).create_grade_sheet(groups, lab_config, group_grades)
 
 def setup_grades(config, lab_number):
