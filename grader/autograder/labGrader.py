d917c93a26f32628ff2738ff8a9f758df14b3ef3
Jack Garrard <jg2562@nau.edu>
2017-10-16 18:29:39 -0700

    Added testing information being printed out

--- grader/autograder/labGrader.py
+++ grader/autograder/labGrader.py
@@ -7,6 +7,8 @@ class LabGrader():
 
     def get_groups_grades(self, groups):
         grades = {}
+        print("Testing all groups")
         for group in groups:
+            print("Testing group {}".format(group))
             grades[group] = self._suite.run_tests(groups[group].get_code())
         return grades
